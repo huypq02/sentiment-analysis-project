04ca5ebc19232f1ac60d80cea3155d4bed3fdc12
Huy Pham <huypham0297@gmail.com>
2025-10-08 19:00:41 +0700

    feat: create unit test for the current models

--- src/models/logreg_model.py
+++ src/models/logreg_model.py
@@ -29,5 +29,5 @@ class LogisticRegressionModel(SentimentModel):
         """Evaluate Logistic Regression model"""
         y_pred = self.classifier.predict(test_data)
         cm = confusion_matrix(test_labels, y_pred)  # Confusion matrix
-        score = accuracy_score(test_labels, y_pred) # Accuracy score
-        return cm, score
+        accuracy = accuracy_score(test_labels, y_pred) # Accuracy score
+        return cm, accuracy
