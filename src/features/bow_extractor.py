6bd5046286548a9e1d5ea104ab57c119a048e26c
Huy Pham <huypham0297@gmail.com>
2025-10-11 22:51:13 +0700

    fix: add some arguments for intialization for bag of words

--- src/features/bow_extractor.py
+++ src/features/bow_extractor.py
@@ -2,8 +2,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from .base_feature_extractor import BaseFeatureExtractor
 
 class BagOfWordsExtractor(BaseFeatureExtractor):
-    def __init__(self):
-        self.vectorizer = CountVectorizer()
+    def __init__(self, max_features=None, min_df=1, max_df=1.0, stop_words=None, **vectorizer_kwargs):
+        self.vectorizer = CountVectorizer(
+            max_features=max_features,
+            min_df=min_df,
+            max_df=max_df,
+            stop_words=stop_words,
+            **vectorizer_kwargs
+        )
     
     def fit(self, sentences):
         """Fit the Bag-of-Words extractor on training texts."""
