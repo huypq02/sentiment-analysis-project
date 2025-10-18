b127b62396ac68a9f76dcdad96cb294d6c4843e3
Huy Pham <huypham0297@gmail.com>
2025-10-18 22:51:37 +0700

    feat: add logger, spefically for pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,7 @@
 from .train_pipeline import train_main
+from src.utils.logger import setup_logging
+
+logger = setup_logging(__name__)
 
 def predict_main(model=None, feature_test_scaled=None):
     """Make a prediction."""
@@ -6,9 +9,14 @@ def predict_main(model=None, feature_test_scaled=None):
         # 1-7 Implement the model
         model, _, feature_test_scaled, _, _ = train_main()
 
-    # 8. Make a prediction
-    predictions = model.predict(feature_test_scaled)
-    
+    try:
+        # 8. Make a prediction
+        predictions = model.predict(feature_test_scaled)
+
+    except Exception as e:
+        logger.exception(f'Unexpected error in training pipeline: {e}')
+        return None
+
     return predictions
 
 
--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -6,50 +6,66 @@ from src.data.preprocessor import Preprocessor
 from src.features.tfidf_extractor import TFIDFExtractor
 from src.models.logreg_model import LogisticRegressionModel
 from src.utils.load_config import load_config
+from src.utils.logger import setup_logging
+
+logger = setup_logging(__name__)
 
 def train_main(config_path='config/config.yaml'):
     """The training pipeline on the model"""
     # 1. Load config
+    logger.info("Loading configuration...")
     config = load_config(config_path)
     if config is None:
+        logger.error("Failed to load configuration. Exiting.")
         exit(1)
     filename = os.path.join(config['file']['directory'], config['file']['name'])
 
-    # 2. Load data
-    # Define a DataLoader's object
-    loader = DataLoader()
-    # Import dataset
-    df = loader.load_csv(filename)
-    
-    # 3. Preprocessing
-    # Preprocess data
-    preprocessor = Preprocessor()
-    df['reviewText_clean'] = df['reviewText'].apply(preprocessor.preprocess)
-    # Convert df['reviewText_clean'] from tokens to string X
-    texts_cleaned = df['reviewText_clean'].apply(
-        lambda x: ' '.join(x)
-    )
-    labels = df['rating']
+    try:
+        # 2. Load data
+        logger.info("Loading data...")
+        # Define a DataLoader's object
+        loader = DataLoader()
+        # Import dataset
+        df = loader.load_csv(filename)
+        
+        # 3. Preprocessing
+        logger.info("Data preprocessing...")
+        # Preprocess data
+        preprocessor = Preprocessor()
+        df['reviewText_clean'] = df['reviewText'].apply(preprocessor.preprocess)
+        # Convert df['reviewText_clean'] from tokens to string X
+        texts_cleaned = df['reviewText_clean'].apply(
+            lambda x: ' '.join(x)
+        )
+        labels = df['rating']
 
-    # 4. Split
-    # Split dataset into training set and test set
-    X_train, X_test, y_train, y_test = train_test_split(texts_cleaned, labels, 
-                                                        test_size=0.2, random_state=0)
+        # 4. Split
+        logger.info("Splitting dataset...")
+        # Split dataset into training set and test set
+        X_train, X_test, y_train, y_test = train_test_split(texts_cleaned, labels, 
+                                                            test_size=0.2, random_state=0)
 
-    # 5. Extractor Features
-    # TODO: consider loading config of a specific feature
-    extractor = TFIDFExtractor()
-    feature_train = extractor.fit_transform(X_train)
-    feature_test = extractor.transform(X_test)
+        # 5. Extractor Features
+        logger.info("Implementing the extractor feature...")
+        # TODO: consider loading config of a specific feature
+        extractor = TFIDFExtractor()
+        feature_train = extractor.fit_transform(X_train)
+        feature_test = extractor.transform(X_test)
 
-    # 6. Model strategy
-    # TODO: consider loading config of a specific model
-    model = LogisticRegressionModel()
-    # TODO: Consider if-else with the model no need feature scaling
-    feature_train_scaled, feature_test_scaled = model.scale_feature(feature_train, feature_test) # Feature scaling
-    model.train(feature_train_scaled, y_train) # Train data on the model
+        # 6. Model strategy
+        logger.info("Implementing the model...")
+        # TODO: consider loading config of a specific model
+        model = LogisticRegressionModel()
+        # TODO: Consider if-else with the model no need feature scaling
+        feature_train_scaled, feature_test_scaled = model.scale_feature(feature_train, feature_test) # Feature scaling
+        model.train(feature_train_scaled, y_train) # Train data on the model
+
+    except Exception as e:
+        logger.exception(f'Unexpected error in training pipeline: {e}')
+        return None
 
      # 7. Save model and feature extractor
+    logger.info("Saving model and extractor...")
     joblib.dump(model, config['path']['model'])
     joblib.dump(extractor, config['path']['extractor'])
 
